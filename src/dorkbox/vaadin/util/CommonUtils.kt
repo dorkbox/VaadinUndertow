6703932dd8c36158abd0ad81f6f3ea00a30a5dfe
Robinson <robinson@dorkbox.com>
2024-02-13 11:04:26 +0100

    Updated logger (removed KotlinLogging)

--- src/dorkbox/vaadin/util/CommonUtils.kt
+++ src/dorkbox/vaadin/util/CommonUtils.kt
@@ -1,4 +1,20 @@
 /*
+ * Copyright 2023 dorkbox, llc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
  * Copyright 2017 Pronghorn Technology LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,16 +32,20 @@
 
 package dorkbox.vaadin.util
 
-import mu.KLogger
-import mu.KotlinLogging
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import java.io.PrintWriter
 import java.io.StringWriter
 
-inline fun <reified T> T.logger(): KLogger {
+inline fun <reified T> T.logger(): Logger {
     if (T::class.isCompanion) {
-        return KotlinLogging.logger(T::class.java.enclosingClass.name)
+        return LoggerFactory.getLogger(T::class.java.enclosingClass.name)
     }
-    return KotlinLogging.logger(T::class.java.name)
+    return LoggerFactory.getLogger(T::class.java.name)
+}
+
+inline fun <reified T> T.logger(name: String): Logger {
+    return LoggerFactory.getLogger(name)
 }
 
 
